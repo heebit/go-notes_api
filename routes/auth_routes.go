c4b67fa01f592ac8c523942a152a7d52df81a36d
heebit <heebit66@yandex.ru>
2025-07-25 16:34:34 +0500

    fix auth_routes

--- routes/auth_routes.go
+++ routes/auth_routes.go
@@ -6,6 +6,9 @@ import (
 )
 
 func AuthRoutes(r *gin.Engine) {
-	r.POST("/register", controllers.Register)
-	r.POST("/login", controllers.Login)
+	authGroup := r.Group("/auth")
+	{
+		authGroup.POST("/register", controllers.Register)
+		authGroup.POST("/login", controllers.Login)
+	}
 }
